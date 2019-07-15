fafc99f7a025313c936ab16272f1a28fbbbe0d4e
李政 <lizheng@spap.com>
2019-07-15 22:56:37 +0800

    ...

--- src/view/recruits/recruits/search_select.tsx
+++ src/view/recruits/recruits/search_select.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import api, {RecruitParam} from '../../../api';
 import useApi from '../../../hooks/use_api';
 
-function SearchSelect(props: {setShouldRedirect, setSearchState} extends RecruitParam) {
+interface Props extends RecruitParam {redirect: (p: RecruitParam) => void};
 
+function SearchSelect({redirect, ...restProps}: Props) {
+    const {isLoading, error, data} = useApi(api.recruit.getSearchParams);
+    const [state, setState] = useState(restProps);
+
+    if(isLoading) {
+        return <div>Loading...</div>
+    }
+    if(error) {
+        return null
+    }
+    return <div>
+        {data.map(({id, labelName}) => <button onClick={setState({})}>
+            {labelName}
+        </button>)}
+    </div>
 }
